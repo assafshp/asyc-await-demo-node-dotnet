b8f44dbf3d26c9c55577a6a24f6094fe3156adc9
assafshp <assaf.shp@gmail.com>
2021-11-16 15:17:56 +0200

    await node

---
+++ node/index.js
@@ -0,0 +1,16 @@
+(async function main(){
+    console.log('main function started');
+    await sleep();
+    console.log('main function ended');
+})();
+
+async function timeout(ms) {
+    return new Promise(resolve => setTimeout(()=> {
+        console.log('timeout ended');
+        resolve(true);
+    }, ms));
+}
+async function sleep() {
+    return timeout(1000);
+}
+
