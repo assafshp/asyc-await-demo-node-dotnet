57a7d2a456fece1cb4e95ed531efd61980e339e1
assafshp <assaf.shp@gmail.com>
2021-11-16 18:34:30 +0200

    works

---
+++ dotnet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dotnet
+{
+    class Program
+    {
+        
+
+        static async Task Demo() {
+            Console.WriteLine("started");
+            await Task.Delay(1500);
+            Console.WriteLine("ended");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main started!");
+            Demo().Wait();
+            Console.WriteLine("Main ended!");
+        }
+    }
+}
